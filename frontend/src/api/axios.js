bed3bc3f5c7b8ff6fb8deb87ecf39cd4ab3f3d48
Kartavya2005 <kartavyadongre2005@gmail.com>
2026-08-12 17:25:21 +0530

    Configure frontend API client

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,6 @@ const api = axios.create({
     baseURL:
         import.meta.env.VITE_API_URL ||
         "http://localhost:8080/api",
-
     headers: {
         "Content-Type": "application/json",
     },
